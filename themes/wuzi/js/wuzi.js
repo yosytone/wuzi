ed62043802af39dad2c3bc6e617562ce0061cc56
Artem Grigoryev <artem.grigoryev@initlab.ru>
2025-12-08 19:44:32 +0300

    custom theme

---
+++ themes/wuzi/js/wuzi.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * wuzi behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.wuzi = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));

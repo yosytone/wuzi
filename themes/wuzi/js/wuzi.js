263445254fae2ccdf9b42a684f4a0f5bf7e06172
Artem Grigoryev <artem.grigoryev@initlab.ru>
2026-01-29 22:36:26 +0300

    filter animation

--- themes/wuzi/js/wuzi.js
+++ themes/wuzi/js/wuzi.js
@@ -1,81 +1,46 @@
-/**
- * @file
- * wuzi behaviors.
- */
-(function (Drupal) {
-
-  'use strict';
-
-  Drupal.behaviors.wuzi = {
-    attach (context, settings) {
-      //alert('It works!');
-      console.log('It works!');
-
-    }
-  };
-
-} (Drupal));
-
-// bottom mobile filter
 ((Drupal, once, $) => {
   'use strict';
 
-  Drupal.behaviors.wuzi = {
+  Drupal.behaviors.wuziMobileFilter = {
     attach(context, settings) {
-      once('wuzi', 'body', context).forEach(el => {
+      // Инициализация один раз
+      once('wuzi-mobile-filter-init', 'body', context).forEach(() => {
 
-        function closeMobileFilter() {
+        const $wrap = $('.mobile-bottom-filter-wrap');
+        const $filter = $('.mobile-bottom-filter');
 
-            $(".mobile-bottom-filter-wrap").animate({
-                'background-color': 'rgba(0, 0, 0, 1)'
-            }, 300);
-            $(".mobile-bottom-filter").animate({
-                'top': '100%'
-            }, 300, function() {
-                $(".mobile-bottom-filter-wrap").hide();
-            });
-        }
-        function updateFloatMobileFilter() {
-            if ($(".product-wrap").length == 0) {
-                return false;
-            }
-
-            var topFilterWrapPos = $(".product-wrap").offset().top;
-
-            currentScrollPos = $(this).scrollTop();
-
-            if (currentScrollPos > topFilterWrapPos) {
-
-                $(".js-mobile-bottom-sort_filter-wrap").addClass('mobile-filter-button-float');
-
-            } else {
-                $(".js-mobile-bottom-sort_filter-wrap").removeClass('mobile-filter-button-float');
-            }
-
-        }
-        
-        $('.js-mobile-filter-button').click(function() {
-            $(".mobile-bottom-filter-wrap").show();
-            $(".mobile-bottom-filter-wrap").animate({
-                'background-color': 'rgba(0, 0, 0, 1)'
-            }, 300);
-            $(".mobile-bottom-filter").animate({
-                'top': '20%'
-            }, 300);
-
-            const $wrap = $(".mobile-bottom-filter-wrap");
-            const $filter = $(".mobile-bottom-filter");
-            $wrap.show().addClass('is-open');
+        // Открытие
+        once('mobile-filter-open', '.js-mobile-filter-button', context).forEach(button => {
+          button.addEventListener('click', (e) => {
+            e.preventDefault();
+            $wrap.addClass('is-open');
+          });
         });
 
-        $('.js-mobile-bottom-filter-icon').click(closeMobileFilter);
-
-        $('.js-mobile-bottom-filter-wrap').click(function(e) {
-            if ($(e.target).hasClass('mobile-bottom-filter-wrap')) {
-                closeMobileFilter();
-            }
+        // Закрытие по крестику
+        once('mobile-filter-close-icon', '.js-mobile-bottom-filter-icon', context).forEach(icon => {
+          icon.addEventListener('click', () => {
+            $wrap.removeClass('is-open');
+          });
         });
 
+        // Закрытие по клику на подложку
+        once('mobile-filter-close-wrap', '.js-mobile-bottom-filter-wrap', context).forEach(wrap => {
+          wrap.addEventListener('click', (e) => {
+            if (e.target === wrap) {
+              $wrap.removeClass('is-open');
+            }
+          });
+        });
+
+        // Опционально: скрывать при нажатии Esc
+        once('mobile-filter-esc', 'body', context).forEach(() => {
+          document.addEventListener('keydown', (e) => {
+            if (e.key === 'Escape' && $wrap.hasClass('is-open')) {
+              $wrap.removeClass('is-open');
+            }
+          });
+        });
       });
     }
   };
